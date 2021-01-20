90c286ca7ce31e65674ae5f528a1e8d032a95483
Suranga Fernando <suranga.fernando@sine.co>
2021-01-20 16:51:25 +1030

    adding  SFBottomAlertSheetView

--- Sources/SFBottomAlertSheet/SFBottomAlertSheet.swift
+++ Sources/SFBottomAlertSheet/SFBottomAlertSheet.swift
@@ -1,7 +1,4 @@
 struct SFBottomAlertSheet {
     var text = "Hello, World!"
-    
-    static func printText() {
-        print("Helllo World")
-    }
+
 }
---
+++ Sources/SFBottomAlertSheet/SFBottomAlertSheetView.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Suranga Fernando on 20/1/21.
+//
+
+import Foundation
+class SFBottomAlertSheetView {
+    
+    static func showMeSomething() {
+        print("Hello World")
+    }
+}
