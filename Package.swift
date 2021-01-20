e30b15f3ead1c2aef3134484eed6c2dec9c4a580
Suranga Fernando <suranga825@gmail.com>
2021-01-20 22:38:07 +1030

    renaming package

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "SFBottomAlertSheet",
+    name: "SFBottomAlert",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SFBottomAlertSheet",
+            name: "SFBottomAlert",
             targets: ["SFBottomAlertSheet"]),
     ],
     dependencies: [
